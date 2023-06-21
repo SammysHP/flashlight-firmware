bcbfd52c3023ce9a0f2f09b37b8f4fe64c5fe5f5
SammysHP <sven@sammyshp.de>
2023-06-21 20:39:02 +0200

    Add mixed aux LED channel

--- ToyKeeper/spaghetti-monster/chan-rgbaux.c
+++ ToyKeeper/spaghetti-monster/chan-rgbaux.c
@@ -31,5 +31,18 @@ void set_level_auxwht(uint8_t level) {
     rgb_led_set(!(!(level)) * 0b101010);  // red+green+blue, high (or off)
 }
 
+void set_level_auxmix(uint8_t level) {
+    const uint8_t rgb_led_colors[] = {
+        0b00000010,  // 0: red
+        0b00001010,  // 1: yellow
+        0b00001000,  // 2: green
+        0b00101000,  // 3: cyan
+        0b00100000,  // 4: blue
+        0b00100010,  // 5: purple
+    };
+    uint8_t color = cfg.channel_mode_args[cfg.channel_mode] / 43;
+    rgb_led_set(!!level * rgb_led_colors[color]);
+}
+
 bool gradual_tick_null(uint8_t gt) { return true; }  // do nothing
 
--- ToyKeeper/spaghetti-monster/chan-rgbaux.h
+++ ToyKeeper/spaghetti-monster/chan-rgbaux.h
@@ -10,6 +10,7 @@ void set_level_auxcyn(uint8_t level);
 void set_level_auxblu(uint8_t level);
 void set_level_auxprp(uint8_t level);
 void set_level_auxwht(uint8_t level);
+void set_level_auxmix(uint8_t level);
 
 bool gradual_tick_null(uint8_t gt);
 
